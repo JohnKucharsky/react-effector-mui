644b207629800117aa12adf5ddc689fdcc05fcd8
ivan <vannok@live.com>
2025-01-03 21:50:21 +0300

    added env, config changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,28 @@
 import * as path from 'node:path'
 import react from '@vitejs/plugin-react'
-import { defineConfig, ProxyOptions } from 'vite'
+import { defineConfig, loadEnv, ProxyOptions } from 'vite'
 import { apiRoutes } from './src/common/enums.ts'
 
 // https://vite.dev/config/
 
-const proxy: Record<string, string | ProxyOptions> | undefined = {}
-for (const route of Object.keys(apiRoutes)) {
-  proxy[route] = {
-    target: 'http://jsonplaceholder.typicode.com',
-    changeOrigin: true,
-  }
-}
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: { '@': path.resolve(__dirname, 'src').replace(/\\/g, '/') },
-  },
-  server: {
-    proxy,
-  },
+  const proxy: Record<string, string | ProxyOptions> | undefined = {}
+  for (const route of Object.keys(apiRoutes)) {
+    proxy[route] = {
+      target: env.VITE_API_URL,
+      changeOrigin: true,
+    }
+  }
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: { '@': path.resolve(__dirname, 'src').replace(/\\/g, '/') },
+    },
+    server: {
+      proxy,
+    },
+  }
 })
